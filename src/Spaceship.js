93055eec3b923317f79e805d30921b989e8c8238
hksix <mh.haseeb@gmail.com>
2017-09-13 10:28:04 -0400

    creates spaceship components

---
+++ src/Spaceship.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Spaceship extends Component{
+    static propTypes={
+        name
+    }
+    render() {
+        const {name} = this.props;
+        <div>
+            <h1>{name}</h1>
+        </div>
+    }
+}
+
+export default Spaceship;
