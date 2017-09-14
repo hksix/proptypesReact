4ec07f158473423ad9590eb0ec5a73dcc47d3ce0
hksix <mh.haseeb@gmail.com>
2017-09-14 14:42:52 -0400

    Adds makes matters worse.js

--- src/actions.js
+++ src/actions.js
@@ -1,80 +1,63 @@
-import * as constants from './Constants.js'
+import * as constants from './constants';
 
-let thingy={
-    location:{
-        planets:{
-            '52':{
-                id: 52,
-                name: 'Pluto',
-                resources:{
-                    crystals:1000
-                }
-            },
-            '54':{
-                id: 54,
-                name: 'Earth',
-                resources:{
-                    crystals:1000
-                }
-            },
-        },
-        ships: {
-            '1000':{
-                id: 1000,
-                name:'Enterprise',
-                resources:{
-                    cargo:500
-                }
-            }
+let thingy = {
+  locations: {
+    planets: {
+      '52': {
+        id: 52,
+        name: 'Pluto',
+        resources: {
+          crystals: 1000
         }
+      },
+      '54': {
+        id: 54,
+        name: 'Earth',
+        resources: {
+          crystals: 1000
+        }
+      },
     },
-    crewMembers:{
-        '300':{
-            id: 300,
-            name: 'Leo',
-            rank: 'Capitan',
-            location: 52
+    ships: {
+      '1000': {
+        id: 1000,
+        name: 'Enterprise',
+        cargo: 500,
+        resources: {
         }
+      }
     }
+  },
+  crewMembers: {
+    '300': {
+      id: 300,
+      name: 'Leo',
+      rank: 'That big dude',
+      location: 52
+    }
+  }
 }
 
 
-// Tip #1: Make export constatns for your action type strings
+
+
+
+// Tip #1: Make constants for your action type strings.
+
 /*
-
-this is what we want to produce as an action object
+This is what we want to produce as an action object:
 {
-    type: ADD_MEMBER,
-    name: 'Crusher',
-    rank: 'CEO',
-    id: 1234,
-    location: 'PLANET_THE_SUN'
-}
-
-{
-    type: BEAM_MEMBER,
-    id: 1234,
-    location: 'LOCATION_SHIP'
+  type: ADD_MEMBER,
+  name: 'crusher',
+  rank: 'chief medical officer',
+  id: 12345,
+  location: 54,
 }
 {
-    type: ADD_SHIP,
-    name: 'Enterprise',
-    id: 9890
+  type: BEAM_MEMBER,
+  id: 12345,
+  location: 1000,
 }
-{
-    type: ADD_PLANET,
-    name: 'EARTH',
-    classification: Barren,
-    ID: 10
-    mines: 10
-}
-{
-    type:MINE_CRYSTALS,
-    id: 10
-    //reduce number of crystals
-}
-{
-export const LOCATION_PLANET = 'planets'
 {
   type: ADD_LOCATION,
   locationType: LOCATION_PLANET
@@ -95,46 +78,93 @@ export const LOCATION_PLANET = 'planets'
 }
 */
 
-
-export const createPlanet = (store, id, name, resources)=>{
-    return{
-    type: constants.ADD_LOCATION,
-    locationType:constants.LOCATION_PLANET,
-    id: id,
+export const addMember = (name, rank, id, location) => {
+  return {
+    type: constants.ADD_MEMBER,
     name: name,
-    resources:resources
-    }
-}  
-export const removeResource= (resourceName,howMany, idToRemoveFrom)=>{
-    return{
-        type: constants.REMOVE_RESOURCE,
-        resource: resourceName,
-        howMany: howMany,
-        id: idToRemoveFrom,
-    }
+    rank: rank,
+    id: id,
+    location: location,
+  }
+};
+
+export const beamMember = (id, locationId) => {
+ return {
+    type: constants.BEAM_MEMBER,
+    id: id,
+    location: locationId,
+  }
+};
+
+export const addShip = (name, id, resources) => {
+  return {
+    type: constants.ADD_LOCATION,
+    locationType: constants.LOCATION_SHIP,
+    name: name,
+    id: id,
+    resources: resources
+  }
+};
+
+export const addPlanet = (name, id, resources) => {
+  return {
+    type: constants.ADD_LOCATION,
+    locationType: constants.LOCATION_PLANET,
+    name: name,
+    id: id,
+    resources: resources
+  }
+};
+
+export const removeResource = (resourceName, howMany, idToRemoveFrom) => {
+  return {
+    type: constants.REMOVE_RESOURCE,
+    resource: resourceName,
+    howMany: howMany,
+    id: idToRemoveFrom
+  }
+};
+
+export const addResource = (resourceName, howMany, idToAddTo) => {
+  return {
+    type: constants.ADD_RESOURCE,
+    resource: resourceName,
+    howMany: howMany,
+    id: idToAddTo
+  }
+};
+
+
+/*
+// EXAMPLES OF HOW TO USE. DO NOT PUT IN actions.js
+// JUST SHOWING ON SCREEN FOR DEMO ;P
+store.dispatch(addShip('Enterprise', 100, {}));
+// THIS GOES IN reducers.js
+const locations = (state={ planets: {}, ships: {}}, action) => {
+  switch (action.type) {
+    case ADD_LOCATION:
+      const newState = {...state};
+      if (action.locationType === LOCATION_SHIP) {
+        newState.ships[action.id] = {
+          id: action.id,
+          name: action.name,
+          resources: action.resources
+        }
+      } else {
+        newState.planets[action.id] = {
+          id: action.id,
+          name: action.name,
+          resources: action.resources
+        }
+      }
+      return newState;
+    default:
+      return state;
+  }
 }
+*/
 
-export const addResource= (resourceName,howMany, idToAddFrom)=>{
-    return{
-        type: constants.ADD_RESOURCE,
-        resource: resourceName,
-        howMany: howMany,
-        id: idToAddFrom,
-    }
-}
 
-// export default{
-//     ADD_MEMBER,
-//     BEAM_MEMBER,
-//     ADD_LOCATION,
-//     ADD_RESOURCE,
-//     REMOVE_RESOURCE,
-//     createPlanet,
-
-//     // ADD_SHIP,
-//     // ADD_PLANET,
-//     // MINE_CRYSTALS,
-//     // HOLD_LUAU,
-//     // ADD_CRYSTALS,
-//     // REMOVE_CRYSTALS,
+// export default {
+//   addMember,
 // }
--- src/reducers.js
+++ src/reducers.js
@@ -1,12 +1,11 @@
-import { combineReducers } from 'redux'
-import * as actions from './actions.js';
-import * as constants from './Constants.js';
+import { combineReducers } from 'redux';
+import * as actions from './actions';
+import * as constants from './constants';
 
-
-const defaultLocationState={
-    planets: {},
-    ships: {},
-}
+const defaultLocationsState = {
+  planets: {},
+  ships: {}
+};
 
 export const LOCATIONS = {
     TRANSPORTER_ROOM: 'TRANSPORTER_ROOM',
@@ -15,151 +14,154 @@ export const LOCATIONS = {
     UNKNOWN: 'UNKNOWN'
 }
 
-const locations = (state=defaultLocationState, action)=>{
-    switch(action.type){
-        case constants.ADD_LOCATION:
-            const newState= {...state};
-            if(action.locationType === action.LOCATION_SHIP){
-                newState.ships[action.id] = {
-                    id: action.id,
-                    name: action.name,
-                    resources: action.resources
-                  }
-                } else if (action.locationType === action.LOCATION_PLANET){
-                  newState.planets[action.id] = {
-                    id: action.id,
-                    name: action.name,
-                    resources: action.resources
-                  }
-                }
-                return newState;
-
-            
-            // return[
-            //     ...state,
-            //     {
-            //         // locationType: location.planets,
-            //         name: action.name,
-            //         id: action.id,
-            //         resources:{
-            //             crystals: action.resources},
-            //     }
-            // ]
-        // case actions.MINE_CRYSTALS:
-        //     return state.map((mine)=>{
-        //         if(action.id === mine.id){
-        //             let mined = mine.mines - 1
-        //             return {
-        //                 ...mine,
-        //                 mines: mined
-        //             }
-        //         }else{
-        //             return mine
-        //         }
-        //     })
-        //     case actions.REMOVE_CRYSTALS:
-        //     return state.map( (planet) => (
-        //       (action.id === planet.id)
-        //       ? { ...planet, crystals:  planet.crystals - action.howMany }
-        //       : planet
-        //     ));
-        //   case actions.ADD_CRYSTALS:
-        //     return state.map( (planet) => (
-        //       (action.id === planet.id)
-        //       ? { ...planet, crystals:  planet.crystals + action.howMany }
-        //       : planet
-        //     ));
-        default:
-            return state;
-    }
+const locations = (state=defaultLocationsState, action) => {
+  switch (action.type) {
+    case constants.ADD_LOCATION:
+      let newState = {...state};
+      if (action.locationType === constants.LOCATION_SHIP) {
+        newState.ships[action.id] = {
+          id: action.id,
+          name: action.name,
+          resources: action.resources
+        }
+      } else if (action.locationType === constants.LOCATION_PLANET) {
+        newState.planets[action.id] = {
+          id: action.id,
+          name: action.name,
+          resources: action.resources
+        }
+      }
+      return newState;
+    case constants.ADD_RESOURCE:
+        newState= {...state}
+      let locationThatIWantToChange;
+      for(let locationType in newState){
+          for(let location in locationType){
+              if (location.id === action.id){
+                  locationThatIWantToChange = location;
+              }
+          }
+      }
+    default:
+      return state;
+  }
 }
 
-// const ships = (state=[], action) =>{
-//     switch(action.type){
-//         case actions.ADD_SHIP:
-//             return[
-//                 ...state,
-//                 {
-//                     name: action.name,
-//                     id: action.id,
-//                     crystals: 0
-//                 }
-//             ]
-//             case actions.REMOVE_CRYSTALS:
-//             return state.map( (ship) => (
-//               (action.id === ship.id)
-//               ? { ...ship, crystals:  ship.crystals - action.howMany }
-//               : ship
-//             ));
-//           case actions.ADD_CRYSTALS:
-//             return state.map( (ship) => (
-//               (action.id === ship.id)
-//               ? { ...ship, crystals:  ship.crystals + action.howMany }
-//               : ship
-//             ));
-//         default:
-//             return state;
-//     }
-    
-// }
+/*
+const planets = (state=[], action) => {
+  switch (action.type) {
+    case actions.ADD_PLANET:
+      // do something!
+      return [
+        ...state,
+        {
+          id: action.id,
+          name: action.name,
+          crystals: 5,
+        }
+      ];
+    case actions.REMOVE_CRYSTALS:
+      return state.map( (planet) => (
+        (action.id === planet.id)
+        ? { ...planet, crystals:  planet.crystals - action.howMany }
+        : planet
+      ));
+    case actions.ADD_CRYSTALS:
+      return state.map( (planet) => (
+        (action.id === planet.id)
+        ? { ...planet, crystals:  planet.crystals + action.howMany }
+        : planet
+      ));
+    default:
+      return state;
+  }
+};
+const ships = (state=[], action) => {
+  switch (action.type) {
+    case actions.ADD_SHIP:
+      return [
+        ...state,
+        {
+          id: action.id,
+          name: action.name,
+          crystals: 0,
+        }
+      ]
+    case actions.REMOVE_CRYSTALS:
+      return state.map( (ship) => (
+        (action.id === ship.id)
+        ? { ...ship, crystals:  ship.crystals - action.howMany }
+        : ship
+      ));
+    case actions.ADD_CRYSTALS:
+      return state.map( (ship) => (
+        (action.id === ship.id)
+        ? { ...ship, crystals:  ship.crystals + action.howMany }
+        : ship
+      ));
+    default:
+      return state;
+  }
+}
+*/
 
-// const crewMembers = (state=[], action) =>{
-//     switch(action.type){
-//         case actions.ADD_MEMBER:
-//             // return state.concat({
-//             //     name: action.name,
-//             //     rank: action.rank,
-//             //     id: action.id,
-//             //     location: action.location
-//             // });
-//             //  or use the speard operator this is equlivant to state.concat version
-//             return[
-//                 ...state,
-//                 {
-//                     name: action.name,
-//                     rank: action.rank,
-//                     id: action.id,
-//                     location: action.location
-//                 }
-//             ]
-
-//         case actions.BEAM_MEMBER:
-//             return state.map( (crewMember)=>{
-//                 // if the ID matches, then update location
-//                 if(action.id === crewMember.id){
-//                     // make a copy of the crewMember but changing the location
-//                     let newLocation = crewMember.location;
-//                     if(action.location in LOCATIONS){
-//                         newLocation = action.location;
-//                     }
-//                     return {
-//                         ...crewMember,
-//                         location: newLocation
-//                     }
-//                 }else{
-//                     return crewMember;
-//                 }
-//                 // otherwise return crewMember
-//             });
-            
-//             case actions.HOLD_LUAU:
-//                 return;
-//             default:
-//                 return state
-//     }
-// }
-
-
+/*
+const crewMembers = (state=[], action) => {
+  switch (action.type) {
+    case actions.ADD_MEMBER:
+      // *** Very nice, immutable way to add on to an array!
+      // return state.concat({
+      //   name: action.name,
+      //   rank: action.rank,
+      //   id: action.id,
+      //   location: action.location
+      // });
+      // Equivalent to state.concat version,
+      // bit using the array spread operator
+      return [
+        ...state,
+        {
+          name: action.name,
+          rank: action.rank,
+          id: action.id,
+          location: action.location,
+        }
+      ]
+    case actions.BEAM_MEMBER:
+      return state.map( (crewMember) => {
+        // if the id matches, then update the location
+        if (action.id === crewMember.id) {
+          // make a copy(?) of the crewMember
+          // but changing the location
+          // if (action.location in LOCATIONS) {
+          // }
+          let newLocation = crewMember.location;
+          if (action.location in LOCATIONS) {
+            newLocation = action.location;
+          }
+          // *** Nice looking longer version of Object "cloning"
+          // return Object.assign({}, crewMember, {
+          //   location: newLocation
+          // });
+          // This is equivalent to the Object.assign version
+          return {
+            ...crewMember,
+            location: newLocation
+          }
+          // DO NOT DO THIS THING HERE BELOW.
+          // crewMember.location = action.location;
+          // return crewMember;
+        } else {
+          return crewMember;
+        }
+        // otherwise, just return the crewMember
+      });
+      break;
+    default:
+      return state;
+  }
+}
+*/
 export default combineReducers({
-    locations,
-    // ships,
-    // crewMembers,
-    
+  locations,
 });
-
-
-// store.dispatch({
-//     type: ADD_SHIP,
-//     name: "dread",
-//     id: 99999,
-// })
